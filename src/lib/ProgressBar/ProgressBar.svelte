aae341a02f1e06cb8eead949a5b64ad05c46c67a
Tropical <42101043+Tropix126@users.noreply.github.com>
2021-10-25 00:03:24 -0500

    convert non-logical properties

---
+++ src/lib/ProgressBar/ProgressBar.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    let className = "";
+    export { className as class };
+
+    let element: HTMLElement;
+
+    export const getElement = () => element;
+</script>
+
+<div class="{className ?? ''}" bind:this={element} {...$$restProps}></div>
+
+<style lang="scss">
+    @use "./ProgressBar";
+</style>
