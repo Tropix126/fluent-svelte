318263d1cfb34746ca7188837840aff259bd43a1
Tropical <42101043+Tropix126@users.noreply.github.com>
2021-12-30 16:13:05 -0600

    ComboBoxItem refactor

--- src/lib/ContentDialog/ContentDialog.svelte
+++ src/lib/ContentDialog/ContentDialog.svelte
@@ -1,51 +1,61 @@
 <script lang="ts">
 	import { createEventDispatcher } from "svelte";
+	import { get_current_component } from "svelte/internal";
 	import { fade, scale } from "svelte/transition";
 	import { circOut } from "svelte/easing";
-	import { uid, focusTrap, getCSSDuration } from "../internal";
+	import { uid, focusTrap, getCSSDuration, createEventForwarder } from "$lib/internal";
 
-	import TextBlock from "../TextBlock/TextBlock.svelte";
+	import { TextBlock } from "$lib";
 
-	/** Determines whether the dialog is open or not */
+	/** Determines whether the dialog is open or not. */
 	export let open: boolean = false;
 
-	/** Title text displayed as the dialog header */
+	/** Title text displayed as the dialog header. */
 	export let title: string = undefined;
 
-	/** Specifies the visual size of the dialog */
+	/** Specifies the visual size of the dialog. */
 	export let size: "standard" | "max" | "min" = "standard";
 
 	/** Determines whether the dialog can be conventially closed using the escape key. */
 	export let closable = true;
 
-	/** Determines the node the dialog should be appended to */
+	/** Determines the node the dialog should be appended to. */
 	export let append: HTMLElement = undefined;
 
-	/** Determines if the dialog should darken the contents behind it */
+	/** Determines if the dialog should darken the contents behind it. */
 	export let darken = true;
 
-	/** Specifies a custom class name for the dialog */
+	/** Specifies a custom class name for the dialog. */
 	let className = "";
 	export { className as class };
 
+	/** Obtains a bound DOM reference to the inner dialog element. */
+	export let element: HTMLElement = null;
+
+	/** Obtains a bound DOM reference to the dialog's backdrop container element. */
+	export let backdropElement: HTMLElement = null;
+
+	/** Obtains a bound DOM reference to the dialog's inner body element. */
+	export let bodyElement: HTMLElement = null;
+
+	/** Obtains a bound DOM reference to the dialog's footer element. */
+	export let footerElement: HTMLElement = null;
+
+	const forwardEvents = createEventForwarder(get_current_component(), ["open", "close"]);
 	const dispatch = createEventDispatcher();
-
-	let element: HTMLElement;
-
-	export const getElement = () => element;
-
 	const titleId = uid("fds-dialog-title-");
 	const bodyId = uid("fds-dialog-body-");
 
+	$: if (!open) dispatch("close");
+
 	function mountDialog(node: HTMLDivElement) {
+		dispatch("open");
 		if (append) append.appendChild(node);
 		node.focus();
-		dispatch("open");
 	}
 
 	function close() {
 		open = false;
-		dispatch("close");
 	}
 
 	function handleEscapeKey({ key }: KeyboardEvent) {
@@ -64,36 +74,24 @@
 		transition:fade={{ duration: getCSSDuration("--fds-control-faster-duration") }}
 		use:mountDialog
 		use:focusTrap
+		bind:this={backdropElement}
 	>
 		<div
-			on:click
-			on:blur
-			on:focus
-			on:dblclick
-			on:contextmenu
-			on:mousedown
-			on:mouseup
-			on:mouseover
-			on:mouseout
-			on:mouseenter
-			on:mouseleave
-			on:keypress
-			on:keydown
-			on:keyup
+			use:forwardEvents
 			class="content-dialog size-{size} {className ?? ''}"
+			role="dialog"
+			aria-modal="true"
+			aria-labelledby={title && titleId}
+			aria-describedby={bodyId}
 			bind:this={element}
 			transition:scale={{
 				duration: getCSSDuration("--fds-control-fast-duration"),
 				start: 1.05,
 				easing: circOut
 			}}
-			role="dialog"
-			aria-modal="true"
-			aria-labelledby={titleId}
-			aria-describedby={bodyId}
 			{...$$restProps}
 		>
-			<div class="content-dialog-body" id={bodyId}>
+			<div class="content-dialog-body" id={bodyId} bind:this={bodyElement}>
 				{#if title}
 					<TextBlock variant="subtitle" class="content-dialog-title" id={titleId}>
 						{title}
@@ -102,7 +100,7 @@
 				<slot />
 			</div>
 			{#if $$slots.footer}
-				<footer class="content-dialog-footer">
+				<footer class="content-dialog-footer" bind:this={footerElement}>
 					<slot name="footer" />
 				</footer>
 			{/if}
