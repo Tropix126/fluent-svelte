c41b0de61032f9d45c7a068340ead2ee84fce340
Tropical <42101043+Tropix126@users.noreply.github.com>
2021-12-31 21:51:08 -0600

    add comment menuflyoutsurface

--- src/lib/MenuFlyout/MenuFlyoutSurface.svelte
+++ src/lib/MenuFlyout/MenuFlyoutSurface.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+    /** Specifies a custom class name for the surface. */
 	let className = "";
 	export { className as class };
 
+    /** Obtains a bound DOM reference to the surface element. */
 	export let element: HTMLUListElement = null;
 
 	let animationComplete = false;
