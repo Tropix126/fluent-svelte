39a1a8072fb83d6aef699546b1fb3b27205b7885
Tropical <42101043+Tropix126@users.noreply.github.com>
2022-04-10 00:26:16 -0500

    use svelte:element with TextBlock

--- src/lib/TextBlock/TextBlock.svelte
+++ src/lib/TextBlock/TextBlock.svelte
@@ -10,47 +10,31 @@
 		| "titleLarge"
 		| "display" = "body";
 
+    /** Overrides the default HTML tag of the block's element with your own tag. */
+    export let tag = undefined;
+
 	/** Specifies a custom class name for the text block */
 	let className = "";
 	export { className as class };
 
 	/** Obtains a bound DOM reference to the text's element. */
 	export let element: HTMLElement = null;
+
+    const tags = {
+        caption: "caption",
+        body: "body",
+        bodyStrong: "bodyStrong",
+        bodyLarge: "bodyLarge",
+        subtitle: "subtitle",
+        title: "title",
+        titleLarge: "titleLarge",
+        display: "display",
+    }
 </script>
 
-{#if variant === "caption"}
-	<span class="text-block type-caption {className}" bind:this={element} {...$$restProps}>
-		<slot />
-	</span>
-{:else if variant === "body"}
-	<span class="text-block type-body {className}" bind:this={element} {...$$restProps}>
-		<slot />
-	</span>
-{:else if variant === "bodyStrong"}
-	<h6 class="text-block type-body-strong {className}" bind:this={element} {...$$restProps}>
-		<slot />
-	</h6>
-{:else if variant === "bodyLarge"}
-	<h5 class="text-block type-body-large {className}" bind:this={element} {...$$restProps}>
-		<slot />
-	</h5>
-{:else if variant === "subtitle"}
-	<h4 class="text-block type-subtitle {className}" bind:this={element} {...$$restProps}>
-		<slot />
-	</h4>
-{:else if variant === "title"}
-	<h3 class="text-block type-title {className}" bind:this={element} {...$$restProps}>
-		<slot />
-	</h3>
-{:else if variant === "titleLarge"}
-	<h2 class="text-block type-title-large {className}" bind:this={element} {...$$restProps}>
-		<slot />
-	</h2>
-{:else if variant === "display"}
-	<h1 class="text-block type-display {className}" bind:this={element} {...$$restProps}>
-		<slot />
-	</h1>
-{/if}
+<svelte:element this={tag ? tag : tags[variant]} class="text-block type-{variant} {className}" bind:this={element} {...$$restProps}>
+    <slot />
+</svelte:element>
 
 <style lang="scss">
 	@use "./TextBlock";
